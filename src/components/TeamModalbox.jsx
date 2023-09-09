5e68942716d2150645e76f50d25c0821dcc992b9
MVK2803 <mathewvinukariath@gmail.com>
2023-09-09 21:35:48 +0530

    Minor Bug Fixes for team section

--- src/components/TeamModalbox.jsx
+++ src/components/TeamModalbox.jsx
@@ -5,8 +5,8 @@ import TeamCard from "../components/TeamCard";
 function Team() {
   console.log("hello");
   return (
-    <div className="relative font-dm-sans  flex flex-col overflow-auto " style={{height: '75vh'}}>
-      <div className="w-11/12 space-y-6 text-center self-center flex flex-col bg-[#C9D9E8]" >
+    <div className="relative font-dm-sans  flex flex-col overflow-auto  p-5" style={{height: '75vh'}}>
+      <div className="w-11/12 space-y-6 text-center self-center flex flex-col  bg-[#C9D9E8]" >
         <h1 className="inline font-[700] text-[75px] bg-gradient-to-tr from-[#0597F2] to-[#6F04D9] bg-clip-text text-transparent">
         Team
         </h1>
@@ -23,14 +23,14 @@ function Team() {
 export default function Modal(props) {
   return (
     <div className="static ">
-      <div className="fixed top-20 right-20 left-20 z-20 flex justify-center ">
+      <div className="fixed top-20 right-20 left-20 z-20  flex justify-center ">
           <div className="mx-4 my-4 bg-[#C9D9E8]">
               <div className="flex justify-end bg-[#C9D9E8]">
                   <button onClick={() => props.toggler()} className="border-2 text-red-900 px-2 m-2">
                     X
                   </button>
               </div>
-              <div className=" bg-[#C9D9E8] ">
+              <div className=" m bg-[#C9D9E8] ">
                   <Team />
               </div>
           </div>
