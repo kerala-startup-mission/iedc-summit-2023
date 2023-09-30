61cbcbbf66d52268bdeacd4eab6fa4d8ffe36206
MVK2803 <mathewvinukariath@gmail.com>
2023-09-30 11:46:27 +0530

    Major UI Updates In Event Section+Added Fiiltering

--- src/components/Tickets.jsx
+++ src/components/Tickets.jsx
@@ -10,7 +10,7 @@ function Ticket(props) {
     squares.push(
       <div
         key={i}
-        className="bg-white w-[15px] h-[15px] rounded-[15px] mr-2"
+        className="shadow-inner  border-2 bg-zinc-200 w-[15px] h-[15px] rounded-[15px] mr-2"
       ></div>
     );
   }
@@ -18,51 +18,60 @@ function Ticket(props) {
   return (
     <div
       className="flex-none mb-6  overflow-hidden relative flex flex-col h-[622px] 
-    bg-[#3866F2] hover:bg-gradient-to-br from-[#1687F2] to-[#5D25E1]
-    w-[365.82px] rounded-[30px]"
+    bg-white
+    w-[350px] rounded-[30px] "
     >
-      <div className="h-[234px] bg-white  self-center m-[19px]  w-11/12 rounded-[30px]">
+      <div className="h-[50%]  shadow-2xl shadow-blue-100  border border-2 rounded-[20px]  w-full self-center     rounded-t-[30px]">
         <img
           src={eventImg}
           alt="mathew"
-          className="rounded-[30px] w-full h-full "
+          className=" rounded-[20px] w-full h-full  "
         />
       </div>
-      <div className="self-center flex flex-row bg-lime- ">
-        <div className="w-[38px] rotate-180  overflow-hidden">
-          <div className="w-[75px] h-[75px] bg-white rounded-[75px]"></div>
+      
+      <div className="h-[50%] ">
+        <div className=" flex flex-row h-[25%] items-center  mx-auto">
+          <div className="w-[38px]  rotate-180  overflow-hidden">
+            <div className="shadow-inner  w-[75px] bg-zinc-200 h-[75px] bg-white rounded-[75px]"></div>
+          </div>
+          <div className="ml-2 my-auto mx-auto flex">{squares}</div>
+          <div className="w-[38px]   overflow-hidden">
+            <div className="shadow-inner w-[75px] bg-zinc-200 h-[75px] bg-white rounded-[75px]"></div>
+          </div>
         </div>
-        <div className="ml-2 my-auto mx-auto flex">{squares}</div>
-        <div className="w-[38px]   overflow-hidden">
-          <div className="w-[75px] h-[75px] bg-white rounded-[75px]"></div>
-        </div>
-      </div>
-      <div className="font-dm-sans">
-        <div className="w-11/12 mx-auto  h-fit items-center  mx-auto">
-          <h1 className="font-[900] text-3xl leading-tight">{eventName}</h1>
-          <p className=" font-[300] text-[18px]">{eventDescription}</p>
-        </div>
-        <div className="justify-center flex">
-          {eventButton == 0 ? (
-            <a
-              href={eventLink}
-              target="_blank"
-              className="text-center font-[700] absolute bottom-5 text-xl font-extrabold w-[151px] h-[60px] bg-[#FFFFFF] shadow-md rounded-[30px] flex items-center justify-center hover:bg-[#E1F3FE]"
-              rel="noreferrer"
-            >
-              {buttonTitleArray[0]}
-            </a>
-          ) : (
-            <a
-              
-              
-              className="text-center font-[700] absolute bottom-5 text-xl font-extrabold w-[151px] h-[60px]  shadow-md rounded-[30px] flex items-center justify-center"
-              rel="noreferrer"
-            >
-              {buttonTitleArray[eventButton]}
-            </a>
-          )}
-        </div>
+        <div className="h-[75%]   border-t-0 rounded-b-[30px]">
+          <div className="  h-3/4 p-2 w-full  items-center  mx-auto">
+          <h1 className="font-[900] text-left text-2xl  leading-tight">
+              {eventName}
+            </h1>
+            <p className=" font-[300] block text-md ">
+              {eventDescription}
+            </p></div>
+            <div className="h-1/4  items-center  mx-auto">
+            <div className="justify-center flex">
+              {eventButton == 0 ? (
+                <a
+                  href={eventLink}
+                  target="_blank"
+                  className="text-center bg-indigo-200 font-[500] absolute bottom-5 text-md font-extrabold w-[151px] p-3 bg-[#FFFFFF] shadow-md rounded-[30px] flex items-center justify-center hover:bg-[#E1F3FE]"
+                  rel="noreferrer"
+                >
+                  {buttonTitleArray[0]}
+                </a>
+              ) : (
+                <a
+                  
+                  
+                  className="text-center font-[600] absolute bottom-5 text-md font-extrabold w-[151px] h-[60px]  shadow-md rounded-[30px] flex items-center justify-center"
+                  rel="noreferrer"
+                >
+                  {buttonTitleArray[eventButton]}
+                </a>
+              )}
+            </div>
+            </div>
+            </div>
+        
       </div>
     </div>
   );
--- src/sections/Events.jsx
+++ src/sections/Events.jsx
@@ -1,14 +1,35 @@
 import Tickets from "../components/Tickets";
-
+import { useState } from "react";
 function Events(props) {
   const { title, button, sectionRef, eventData, eventDescription } = props;
+  
+  const [activeLink, setActiveLink] = useState('1');
+  
+
+  const links = [
+    { id: '1', text: 'Road To Summit' },
+    { id: '2', text: 'Everything is Entrepreneurship' },
+    { id: '3', text: 'Industry of future' },
+    { id: '4', text: 'Startup Symposium' },
+    // Add more links here as needed
+  ];
+
+  
+
+  const handleLinkClick = (linkId) => {
+    setActiveLink((linkId));
+  };
+
 
   return (
+    <div className="mt-4 bg-zinc-200 w-full flex justify-center">
+
+    
     <div
       ref={sectionRef}
-      className="  relative space-y-10 font-dm-sans  lg:w-10/12 w-11/12  flex flex-col l"
+      className="  relative space-y-10 font-dm-sans  lg:w-10/12 w-11/12  flex flex-col "
     >
-      <div className="absolute overflow-hidden -z-10 opacity-5 text-[#0597F2] xl:top-[-100px] lg:top-[-70px] md:top-[-20px] hidden-xs top-0 w-full space-y-6 text-center self-center">
+      <div className=" absolute overflow-hidden z-10 opacity-5 text-[#0597F2] xl:top-[-100px] lg:top-[-70px] md:top-[-20px] hidden-xs top-0 w-full space-y-6 text-center self-center">
         <h1 className="xl:text-[250px] lg:text-[200px] md:text-[150px] sm:text-[120px] whitespace-nowrap font-bold">
           {title}
         </h1>
@@ -19,13 +40,45 @@ function Events(props) {
           {eventDescription}
         </p>
       </div>
-      <div className="flex md:flex-wrap overflow-x-auto scroll-smooth gap-16 md:gap-0 pb-8 md:justify-evenly horiz-scroll">
+      <div className="w-full flex flex-col">
+      <div>
+      <div className="flex flex-col p-0 w-full  custom2:p-10">
+      <nav>
+        <div className=" w-full">
+          <div className=" items-start text-2xl flex flex-wrap flex-row  font-darker-grotesque font-semibold custom1:flex-row custom2:flex-col custom2:items-center custom1:items-start">
+            {links.map((link) => (
+              <button
+                key={link.id}
+                onClick={() => handleLinkClick(link.id)}
+                className={`w-1/4 nav-link ml-0 p-2 custom1:ml-0 ${
+                  activeLink === link.id
+                    ? 'text-[#3866F2] bg-white rounded-md shadow-md'
+                    : 'text-[#858585]'
+                }`}
+              >
+                {link.text}
+              </button>
+            ))}
+            <hr className="w-full border border-[1px] opacity-30 border-black"></hr>
+          </div>
+        </div>
+      </nav>
+    
+    </div>
+    </div>
+      
+    
+
+        
+      </div>
+      <div className="flex md:flex-wrap gap-16 md:gap-0 pb-8 md:justify-evenly ">
         {eventData &&
           eventData.length > 0 &&
           eventData.map((ticket, index) => (
-            <Tickets
+            ticket.sectionId === parseInt(activeLink)  && <Tickets
               key={index}
               
+
               eventName={ticket.ticketTitle}
               eventImg={ticket.ticketImg}
               eventDescription={ticket.ticketDescription}
@@ -34,6 +87,7 @@ function Events(props) {
             />
           ))}
       </div>
+    </div>
     </div>
   );
 }
