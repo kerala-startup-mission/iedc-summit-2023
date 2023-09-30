c12793849ed841f019f421f7ec6ee27d3439d598
MVK2803 <mathewvinukariath@gmail.com>
2023-09-30 23:05:27 +0530

    Visual Chnages+Padding Issues

--- src/components/callsCard.jsx
+++ src/components/callsCard.jsx
@@ -5,7 +5,7 @@ function callCard(props) {
     props;
 
   return (
-    <div className="flex-none mb-6  overflow-hidden relative flex flex-col h-[451px] bg-[#2c2c2c]  border-[1px] border-[#3866F266] shadow-lg hover:shadow-xl  w-[340px] rounded-lg lg:rounded-2xl ">
+    <div className="flex-none mb-12  overflow-hidden relative flex flex-col h-[451px] bg-[#2c2c2c]  border-[1px] border-[#3866F266] shadow-lg hover:shadow-xl  w-[340px] rounded-lg lg:rounded-2xl ">
       <img
         src={eventImg}
         className=" shadow-lg h-full flex-1 h-1/2 bg-orange-300   rounded-t-md object-cover"
--- src/sections/landingPage.jsx
+++ src/sections/landingPage.jsx
@@ -65,7 +65,7 @@ function LandingPage({ sectionRef }) {
             className="border-2 border-[#0698F3] rounded-[30px] px-4 py-2 text-2xl text-[#0698F3] font-bold hover:bg-gradient-to-br from-[#0597F2] to-[#6F04D9] hover:text-white"
             onClick={toggleModal}
           >
-            OFFICIAL MERCH
+            Official Merchandise
           </button>
         </div>
       </div>
